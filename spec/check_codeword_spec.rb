9f978b68254c646666ad9ffc790214daa2f6daf8
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-05-17 15:16:25 +0100

    wrote methods and tests for exercise 2

---
+++ spec/check_codeword_spec.rb
@@ -0,0 +1,16 @@
+require 'check_codeword'
+
+RSpec.describe "check_codeword method" do
+  it "returns correct if the codeword is correct" do
+    expect(check_codeword("horse")).to eq "Correct! Come in."
+  end
+
+  it "returns close when first and last letters are correct" do
+    expect(check_codeword("house")).to eq "Close, but nope."
+  end
+
+  it "returns wrong if codeword is not correct or close to being correct" do
+    expect(check_codeword("dog")).to eq "WRONG!"
+  end
+
+end
